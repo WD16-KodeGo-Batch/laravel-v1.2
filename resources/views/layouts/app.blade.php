652ff1f906c7402194253d144b05a9034455563e
Rodrigo Lopez <scdtp2019@gmail.com>
2022-11-12 09:25:04 +0800

    Add motorcyle svg file.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                 <div><img src="/svg/rbslgamer4.svg"></div>
+                 <div><img src="/svg/rbslgamer4.svg" style="max-height:100px"></div>
                  <div>RBSLPhotogram</div>    
                 
                 </a>

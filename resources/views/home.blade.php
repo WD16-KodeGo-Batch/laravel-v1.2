80b26395881fdf78bf63815e2701a6ad8c9001e8
Rodrigo Lopez <scdtp2019@gmail.com>
2022-11-12 09:57:00 +0800

    Add profile picture placeholder

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <!-- <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
@@ -17,6 +17,12 @@
                     {{ __('You are logged in!') }}
                 </div>
             </div>
+        </div>
+    </div> -->
+    <div class="row">
+        <div class="col-3">
+            <img src="https://raw.githubusercontent.com/WD16-KodeGo-Batch/image-sources/main/dog-photo-withApp5-small.png" class="rounded-circle" style="height:200px; width:200px;">
+            <div class="col-9"></div>
         </div>
     </div>
 </div>
